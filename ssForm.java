dd7dd5e53603452cf384579ce4dd2d1f280ff831
Akila Kushantha <akila.kushantha@gmail.com>
2023-12-16 20:33:49 +0530

    form created

--- ssForm.java
+++ ssForm.java
@@ -6,16 +6,39 @@ public class ssForm {
     private JPanel mainPanel;
     private JButton gotoServicesButton;
     private JTabbedPane tabbedPane1;
-    private JTabbedPane tabbedPane2;
     private JButton gotoTechnicianButton;
+    private JTabbedPane tabbedPane2;
+    private JTextField sevID;
+    private JTextField sevName;
+    private JTextField sevModel;
+    private JTextField sevDate;
+    private JTextField sevType;
+    private JTextField sevStatus;
+    private JTextField sevNameAdd;
+    private JTextField sevModelAdd;
+    private JTextField sevDateAdd;
+    private JTextField sevTypeAdd;
+    private JButton findID;
     private JButton btn1;
 
     public ssForm() {
-        btn1.addActionListener(new ActionListener() {
+
+
+        gotoServicesButton.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
-                JOptionPane
+                tabbedPane1.setSelectedIndex(1);
             }
         });
+
+
+        }
+
+    public static void main(String[] args) {
+        JFrame frame = new JFrame("ssForm");
+        frame.setContentPane(new ssForm().mainPanel);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.pack();
+        frame.setVisible(true);
     }
 }
