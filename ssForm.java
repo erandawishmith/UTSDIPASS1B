3ec29d63686d2b3a22e86f8f8c723d393f19d52d
Akila Kushantha <akila.kushantha@gmail.com>
2023-12-17 03:21:49 +0530

    services form created

--- ssForm.java
+++ ssForm.java
@@ -7,7 +7,6 @@ public class ssForm extends Records {
     private JButton gotoServicesButton;
     private JTabbedPane tabbedPane1;
     private JButton gotoTechnicianButton;
-    private JTabbedPane tabbedPane2;
     private JTextField sevID;
     private JTextField sevName;
     private JTextField sevModel;
@@ -22,10 +21,19 @@ public class ssForm extends Records {
     private JButton submitButton;
     private JButton genReport;
     private JTextArea reporttxt;
-    private JTextArea sevEdit;
+    private JButton editbtn;
+    private JTabbedPane tabbedPane3;
+    private JTextField textField1;
+    private JTextField textField4;
+    private JButton SHOWButton;
+    private JTextField textField5;
+    private JTextField textField2;
+    private JTextField textField3;
+    private JTabbedPane tabbedPane2;
     private JButton btn1;
 
     private Services servicesList = new Services(); // Assuming Services is a class
+    private Technicians technicianList = new Technicians(); // Assuming Technicians is a class
 
     public ssForm() {
 //        go to services tab
@@ -69,13 +77,63 @@ public class ssForm extends Records {
             @Override
             public void actionPerformed(ActionEvent e) {
                 int id = Integer.parseInt(sevID.getText());
-                Service service = (Service) find(id);
+                Service service = servicesList.find(id);
+                System.out.println(id);
 
-                if (service != null){
-                    sevEdit.setText(service.getName());
+                if (service != null) {
+                    String model = service.model;
+                    String date = service.date;
+                    ServiceType type = service.serviceType;
+                    Status status = service.status;
+                    sevName.setText(service.getName());
+                    sevModel.setText(model);
+                    sevDate.setText(date);
+                    sevType.setText(type.toString());
+                    sevStatus.setText(status.toString());
+//
+
+                } else {
+                    JOptionPane.showMessageDialog(mainPanel, "Service not found with ID: " + id);
                 }
             }
         });
+        editbtn.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                try {
+                    int id = Integer.parseInt(sevID.getText());
+                    Service service = servicesList.find(id);
+
+                    if (service != null) {
+                        // Display the existing service details
+//                        System.out.println("Existing service details:");
+//                        System.out.println(service.toString());
+
+                        String customerName = sevName.getText();
+                        String vehicleModel = sevModel.getText();
+                        String date = sevDate.getText();
+                        String serviceType = sevType.getText();
+                        System.out.println(customerName);
+
+                        // Create a new Service instance with updated values
+                        Service newService = new Service(id, customerName, vehicleModel, date, ServiceType.FULL_SERVICE, Status.Booked);
+
+                        // Update the service in the servicesList
+                        Service updatedService = servicesList.updateService(id-1, newService);
+
+                        // Optional: Display a confirmation message
+                        JOptionPane.showMessageDialog(mainPanel, "Service edited successfully!");
+                        servicesList.show();
+                    } else {
+                        JOptionPane.showMessageDialog(mainPanel, "Service not found with ID: " + id);
+                    }
+                } catch (NumberFormatException ex) {
+                    JOptionPane.showMessageDialog(mainPanel, "Invalid ID. Please enter a valid integer.");
+                }
+            }
+        });
+
+
     }
 
     public static void main(String[] args) {
@@ -85,4 +143,11 @@ public class ssForm extends Records {
         frame.pack();
         frame.setVisible(true);
     }
+
+
+
+
+    //Technician
+
+
 }
