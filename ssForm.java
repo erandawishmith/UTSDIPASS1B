390bad3df06a981e4df67138f893d039d8da2f61
erandawishmith <03eranda@gmail.com>
2023-12-16 19:39:17 +0530

    hi

---
+++ ssForm.java
@@ -0,0 +1,21 @@
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class ssForm {
+    private JPanel mainPanel;
+    private JButton gotoServicesButton;
+    private JTabbedPane tabbedPane1;
+    private JTabbedPane tabbedPane2;
+    private JButton gotoTechnicianButton;
+    private JButton btn1;
+
+    public ssForm() {
+        btn1.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                JOptionPane
+            }
+        });
+    }
+}
