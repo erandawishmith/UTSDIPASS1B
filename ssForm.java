89724945d8a03b75e893a08f7190d6ebd9855412
Akila Kushantha <akila.kushantha@gmail.com>
2023-12-25 17:00:26 +0530

    qwe

--- ssForm.java
+++ ssForm.java
@@ -3,6 +3,7 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
 public class ssForm extends Records {
+    int i;
     private JPanel mainPanel;
     private JButton gotoServicesButton;
     private JTabbedPane tabbedPane1;
